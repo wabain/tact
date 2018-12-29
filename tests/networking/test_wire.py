9c957299b4817710f76aaf5b9be5ecf5f5b149e9
William Bain <bain.william.a@gmail.com>
2018-12-29 16:23:05 -0500

    More wire message testing

--- tests/networking/test_wire.py
+++ tests/networking/test_wire.py
@@ -23,6 +23,17 @@ SERVER_MSG = {
     },
 }
 
+CLIENT_MSG = {
+    'version': '0.1',
+    'msg_id': 100,
+    'msg': {
+        'type': ClientMsgType.NEW_GAME.value,
+        'player': 1,
+        'squares_per_row': 8,
+        'run_to_win': 5,
+    },
+}
+
 
 def test_server_message_build():
     msg = ServerMessage.build(
@@ -34,7 +45,7 @@ def test_server_message_build():
     assert msg == SERVER_MSG
 
 
-def test_server_message_build_invalid_nonce():
+def test_server_message_build_invalid_payload():
     # TODO: refactor to get better error message
     with pytest.raises(voluptuous.MultipleInvalid):
         ServerMessage.build(
@@ -55,7 +66,7 @@ def test_server_message_parse():
     }
 
 
-def test_server_message_parse_invalid_nonce():
+def test_server_message_parse_invalid_payload():
     with pytest.raises(voluptuous.MultipleInvalid):
         ServerMessage.parse(dict(SERVER_MSG, msg=dict(SERVER_MSG['msg'], nonce='100')))
 
@@ -66,6 +77,54 @@ def test_server_message_parse_invalid_version():
     with pytest.raises(voluptuous.MultipleInvalid):
         try:
             ServerMessage.parse(dict(SERVER_MSG, version='3.5'))
+        except voluptuous.MultipleInvalid as exc:
+            errs = exc.errors
+            raise
+
+    assert [e.path for e in errs] == [['version']]
+
+
+def test_client_message_build():
+    msg = ClientMessage.build(
+        ClientMsgType.NEW_GAME, msg_id=100, player=1, squares_per_row=8, run_to_win=5
+    )
+    assert msg == CLIENT_MSG
+
+
+def test_client_message_build_invalid_payload():
+    # TODO: refactor to get better error message
+    with pytest.raises(voluptuous.MultipleInvalid):
+        ServerMessage.build(
+            ClientMsgType.NEW_GAME,
+            msg_id=100,
+            player=20,
+            squares_per_row=8,
+            run_to_win=5,
+        )
+
+
+def test_client_message_parse():
+    msg_type, payload = ClientMessage.parse(CLIENT_MSG)
+    assert msg_type == ClientMsgType.NEW_GAME
+    assert payload == {
+        'type': ClientMsgType.NEW_GAME.value,
+        'player': 1,
+        'squares_per_row': 8,
+        'run_to_win': 5,
+    }
+
+
+def test_client_message_parse_invalid_payload():
+    with pytest.raises(voluptuous.MultipleInvalid):
+        ServerMessage.parse(dict(CLIENT_MSG, msg=dict(CLIENT_MSG['msg'], player=20)))
+
+
+def test_client_message_parse_invalid_version():
+    errs = None
+
+    with pytest.raises(voluptuous.MultipleInvalid):
+        try:
+            ClientMessage.parse(dict(CLIENT_MSG, version='3.5'))
         except voluptuous.MultipleInvalid as exc:
             errs = exc.errors
             raise
