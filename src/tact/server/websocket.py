b35047da2d5a80257d4de0059f667ba78ba217d6
William Bain <bain.william.a@gmail.com>
2018-12-29 20:43:12 -0500

    Towards server WS support

---
+++ src/tact/server/websocket.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractWSManager(ABC):
+    @abstractmethod
+    async def send(self, conn_id: str, msg: str) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def close(self, conn_id: str):
+        raise NotImplementedError
