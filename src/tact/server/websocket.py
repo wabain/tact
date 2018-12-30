19429c2ae0f8e6fdd877830133a169a1bf45daaf
William Bain <bain.william.a@gmail.com>
2018-12-29 20:43:12 -0500

    Towards server WS support

---
+++ src/tact/server/websocket.py
@@ -0,0 +1,20 @@
+"""Base definitions for the websocket interface"""
+import json
+from abc import ABC, abstractmethod
+
+
+class AbstractWSManager(ABC):
+    @abstractmethod
+    async def send(self, conn_id: str, msg: dict) -> None:
+        # This is implemented as a separate method primarily for test
+        # convenience
+        serialized = json.dumps(msg, separators=(',', ':'))
+        self._send_serialized(conn_id, serialized)
+
+    @abstractmethod
+    async def _send_serialized(self, conn_id: str, msg: str) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def close(self, conn_id: str):
+        raise NotImplementedError
